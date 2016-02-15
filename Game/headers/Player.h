d85875dfc20591797669ba43a6772f8729905cd7
Svetlin <svetlin.j@abv.bg>
2016-02-15 16:50:04 +0200

    Test

--- Game/headers/Player.h
+++ Game/headers/Player.h
@@ -7,4 +7,15 @@ class Player{
 	int hp_, attack_, magic_;
 public:
 	Player();
+	int get_name();
+	int get_hp();
+	int get_attack();
+	int get_magic();
+	
+	void set_name();
+	void set_hp();
+	void set_attack();
+	void set_magic();
+	
+	void attack();
 };
