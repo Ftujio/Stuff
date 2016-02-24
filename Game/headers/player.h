cad9d87f41e86b7e8c92417607cfa6440dd4eca4
Svetlin <svetlin.j@abv.bg>
2016-02-24 16:30:22 +0200

    Added default taunts to player

--- Game/headers/player.h
+++ Game/headers/player.h
@@ -3,8 +3,8 @@
 using std::string;
 
 class Player{
-	string name_;
-	int hp_, attack_, magic_;
+	string name_, taunts_[5];
+	int hp_, attack_, magic_, taunt_num_;
 public:
 	Player(string name, int hp, int attack, int magic);
 	Player();
@@ -13,11 +13,13 @@ public:
 	int get_hp()const;
 	int get_attack()const;
 	int get_magic()const;
+	string get_taunt(int taunt_num)const;
 	
 	void set_name(string name);
 	void set_hp(int hp);
 	void set_attack(int attack);
 	void set_magic(int magic);
+	void add_taunt(string taunt);
 	
 	virtual int attack() = 0;
 };
--- Game/player.cc
+++ Game/player.cc
@@ -1,6 +1,9 @@
 #include "headers/player.h"
 
-Player::Player(string name, int hp, int attack, int magic): name_(name), hp_(hp), attack_(attack), magic_(magic){}
+Player::Player(string name, int hp, int attack, int magic): name_(name), hp_(hp), attack_(attack), magic_(magic), taunt_num_(2){
+	taunts_[0] = "N00b!";
+	taunts_[1] = "GET REKT!";
+}
 
 Player::Player(): name_("Joe"), hp_(10), attack_(1), magic_(0){}
 
@@ -20,6 +23,10 @@ int Player::get_magic() const {
 	return magic_;
 }
 
+string Player::get_taunt(int taunt_num)const{
+	return taunts_[taunt_num];
+}
+
 void Player::set_hp(int hp){
 	hp_ = hp;
 }
@@ -30,4 +37,9 @@ void Player::set_attack(int attack){
 
 void Player::set_magic(int magic){
 	magic_ = magic;
+}
+
+void Player::add_taunt(string taunt){
+	taunts_[taunt_num_] = taunt;
+	taunt_num_++;
 }
