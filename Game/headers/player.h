47a57694f84921555587122396fcaa021547ae89
Ftujio <svetlin954@abv.bg>
2016-02-29 22:41:58 +0200

    Grouped player function prototipes by type #h

--- Game/headers/player.h
+++ Game/headers/player.h
@@ -44,21 +44,21 @@ public:
 	Player();
 	
 	string get_name()const;
+	void set_name(string name);
+
 	int get_hp()const;
 	int get_attack()const;
 	int get_magic()const;
+	void set_hp(int hp);
+	void set_attack(int attack);
+	void set_magic(int magic);
+	void add_taunt(string taunt, int set_place);
 
 	string get_taunt(int taunt_num)const;
 
 	int get_xp()const;
 	int get_level()const;
-	
-	void set_name(string name);
-	void set_hp(int hp);
-	void set_attack(int attack);
-	void set_magic(int magic);
-	void add_taunt(string taunt, int set_place);
-	void add_xp(int amount);
+	void add_xp(int amount);	
 	
 	virtual int attack() = 0;
 };
