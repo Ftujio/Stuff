7459057d47f3b225cb7ec569a2aa7eac9ad511a1
Svetlin <svetlin.j@abv.bg>
2016-03-07 16:24:27 +0200

    Initial Commit

---
+++ libs/ParseJson/main.cc
@@ -0,0 +1,11 @@
+#include "iostream"
+#include "cstdlib"
+#include "cstdio"
+
+using namespace std;
+
+int main(){
+	
+
+	return 0;
+}
---
+++ libs/ParseJson/parse.h
@@ -0,0 +1,11 @@
+#include "string"
+
+using std::string;
+
+class Parser{
+	class Arr{};
+	class Element{};
+
+public:
+	Parser(string filename);
+};
