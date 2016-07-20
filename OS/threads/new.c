f9a7fd1d8fa018edc2df47bca85318807dd1b1ee
Fujio <svetlin954@abv.bg>
2016-07-20 18:09:34 +0000

    added new "work" function

--- OS/threads/new.c
+++ OS/threads/new.c
@@ -8,22 +8,53 @@ int p = 0;
 pthread_mutex_t mp;
 pthread_mutex_t m;
 
-void* work(void* arg){
+void* work_a(void* arg){
 	pthread_mutex_lock(&m);
 	while(a > 0){
 		pthread_mutex_unlock(&m);
 	
+		int dig = 0;
 		pthread_mutex_lock(&m);
-		a -= 10;
+		if(a >= 10){
+			a -= 10;
+			dig = 1;
+		}
+		pthread_mutex_unlock(&m);
+		
+		if(dig){
+			pthread_mutex_lock(&mp);
+			p += 10;
+			pthread_mutex_unlock(&mp);
+		}
+		
+		pthread_mutex_lock(&mp);
+	}
+	pthread_mutex_unlock(&m);
+	
+	return NULL;
+}
+
+void* work(void* arg){
+	while(1){
+		pthread_mutex_lock(&m);
+		if(a == 0){
+			pthread_mutex_unlock(&m);
+			break;
+		}
+		pthread_mutex_unlock(&m);
+
+		pthread_mutex_lock(&m);
+		if(a >= 10){
+			a -= 10;
+		} else {
+			break;
+		}
 		pthread_mutex_unlock(&m);
 		
 		pthread_mutex_lock(&mp);
 		p += 10;
 		pthread_mutex_unlock(&mp);
-		
-		pthread_mutex_lock(&mp);
 	}
-	pthread_mutex_unlock(&m);
 	
 	return NULL;
 }
