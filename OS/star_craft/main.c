fca385b3053bb90a82133bdeda4a90418c5f311f
Ftujio <svetlin954@abv.bg>
2016-07-21 15:44:34 +0300

    Added more stuff to the program

--- OS/star_craft/main.c
+++ OS/star_craft/main.c
@@ -13,24 +13,56 @@
 #define SOLDIER_TRAIN_COST 50
 #define SOLDIER_TRAINED_MESSAGE "You wanna peace of me boy?"
 
+// Map and center resources variables
+int map_minerals = MAP_MINERALS, center_minerals = 0;
+
+// Other variables
+int soldier_num = 0;
+int worker_num = WORKER_START_NUM;
+
+// Mutexes
+pthread_mutex_t m_map;
+pthread_mutex_t m_center;
+
 void train(){
 
 }
 
 void transport(){
-
+	printf("SCV N is transporting minerals\n");
+	sleep(2);
 }
 
 void deliver(){
-
+	printf("SCV N delivered minerals to Command center\n");
+	center_minerals += 8;
 }
 
 void mine(){
+	printf("SCV N is mining\n");
+	map_minerals -= 8;
+}
+
+void* work(void* args){
+	
+}
+
+void finish_game(){
 
 }
 
+void run_game(){
+	pthread_mutex_init(&m_map, NULL);
+	pthread_mutex_init(&m_center, NULL);
+
+
+
+	pthread_mutex_destroy(&m_map);
+	pthread_mutex_destroy(&m_center);
+}
+
 int main(int argc, char* argv[]){
-	
+	run_game();
 
 	return 0;
 }
