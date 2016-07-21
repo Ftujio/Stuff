e8d9323a637d88f48661fb788ca52b8fde89d9a2
Ftujio <svetlin954@abv.bg>
2016-07-21 14:41:42 +0300

    Added function prototypes and defined constants

--- OS/star_craft/main.c
+++ OS/star_craft/main.c
@@ -3,6 +3,32 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#define MAP_MINERALS 5000
+
+#define WORKER_START_NUM 5
+#define MAX_WORKERS 200
+#define WORKER_TRAINED_MESSAGE "SCV good to go, sir."
+
+#define SOLDIER_WIN_NUM 20
+#define SOLDIER_TRAIN_COST 50
+#define SOLDIER_TRAINED_MESSAGE "You wanna peace of me boy?"
+
+void train(){
+
+}
+
+void transport(){
+
+}
+
+void deliver(){
+
+}
+
+void mine(){
+
+}
+
 int main(int argc, char* argv[]){
 	
 
