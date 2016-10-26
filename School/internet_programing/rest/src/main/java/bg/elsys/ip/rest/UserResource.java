0d4ef8c558383d20d279d390c331bf25fc4333f9
Ftujio <svetlin954@abv.bg>
2016-10-26 17:59:14 +0300

    .

--- School/internet_programing/rest/src/main/java/bg/elsys/ip/rest/UserResource.java
+++ School/internet_programing/rest/src/main/java/bg/elsys/ip/rest/UserResource.java
@@ -1,5 +1,8 @@
 package bg.elsys.ip.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,6 +14,8 @@ public class UserResource {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getUsers() {
-		return Response.ok(new User(1, "asdf")).build();
+		List<User> users = new ArrayList<>();
+		users.add(new User(1, "asdf"));
+		return Response.ok(users).build();
 	}
 }
